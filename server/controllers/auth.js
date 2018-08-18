06a24f68ca14bbe55b9945e3039df48699cecb45
kneric <brian.pradipta.a@gmail.com>
2018-08-18 23:10:44 +0700

    edit auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,24 +1,23 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-module.exports = {
-  auth : (req, res, next)=> {
-    if (!req.body.token){
-      res.status(401).json({message: 'not authorized'})
-    } else {
-      let loggedInUser = jwt.verify(req.body.token, process.env.secretKey);
+const auth = (req, res, next)=> {
+  if (!req.body.token){
+    res.status(403).json({message: 'not authorized'})
+  } else {
+    let loggedInUser = jwt.verify(req.body.token, process.env.secretKey);
 
-      User.findById(loggedInUser._id)
-      .then(user => {
-        if (!user){
-          res.status(404).json({message: 'user is not registered'})
-        } else {
-          next()
-        }
-      })
-      .catch(err => {
-        res.status(500).json({message: err})
-      })
-    }
+    User.findById(loggedInUser._id)
+    .then(user => {
+      if (!user){
+        res.status(404).json({message: 'user is not registered'})
+      } else {
+        return loggedInUser
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: err})
+    })
   }
-};
+}
+module.exports = auth
--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,6 +1,6 @@
 const jwt = require ('jsonwebtoken');
 const Todo = require('../models/todo');
-const loggedInUser = jwt.verify(req.body.token, process.env.secretKey)
+const auth = require('./auth');
 
 const createTodo = (req, res)=> {
   const {task, dueDate, description, priority} = req.body
